4ead3f1bab8c0e97b0c7514ca57bc82ca790a542
SRC Naamle <mrsurajpatel2003@gmail.com>
2024-12-21 16:31:13 +0530

    make delivery option interective

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOPtion } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -31,7 +31,7 @@ cart.forEach((cartItem)=>{
       deliveryOption = option;
     }
   });
-  
+
   const today = dayjs();
   const deliverydate = today.add(deliveryOption.deliveryDays, 'days');
 
@@ -95,7 +95,8 @@ function deliveryOptionHTML(matchingProduct ,cartItem){
 
     html +=
     `
-    <div class="delivery-option">
+    <div class="delivery-option js-delivery-option" 
+    data-product-id= "${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
         ${isChecked ? 'checked': ''}
         class="delivery-option-input"
@@ -128,3 +129,11 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
   })
 });
 
+
+document.querySelectorAll('.js-delivery-option').forEach((element)=>{
+  element.addEventListener('click', ()=>{
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOPtion(productId, deliveryOptionId);
+    
+  });
+});
