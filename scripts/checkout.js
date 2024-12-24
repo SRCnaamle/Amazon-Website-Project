0b7aa09810417e643a08d4113f070f11aa9a62aa
SRC Naamle <mrsurajpatel2003@gmail.com>
2024-12-24 09:47:11 +0530

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkuot/orderSummary.js';
 import { renderPaymentSummary } from './checkuot/paymentSummary.js';
-import '../data/cart-Class.js';
+
 
 renderOrderSummary();
 renderPaymentSummary();
