15baf57e674b4922e997c43296f91d9af52652e6
SRC Naamle <106827024+SRCnaamle@users.noreply.github.com>
2024-12-25 10:48:28 +0530

    Update paymentSummary.js

--- scripts/checkuot/paymentSummary.js
+++ scripts/checkuot/paymentSummary.js
@@ -7,7 +7,7 @@ export function renderPaymentSummary(){
   let productPriceCents = 0;
   let shippingPriceCents = 0; 
   cart.forEach((cartItem)=>{
-    const product = getProduct(cartItem.productId);
+    const product = loadProducts(getProduct(cartItem.productId));
     productPriceCents += product.priceCents * cartItem.quantity;
     const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
     shippingPriceCents += deliveryOption.priceCents;
